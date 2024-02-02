5e858346209ec4be5ab3f020499d6f93f8dd517d
serhii honcharenko <32honcharenko@gmail.com>
2024-02-02 17:41:18 +0200

    chalk

---
+++ nodeBasics/index.js
@@ -0,0 +1,7 @@
+// Імпорт бібліотеки chalk
+const chalk2 = require('chalk');
+
+// Виведення кольорового тексту в консоль
+console.log(chalk2.blue('Це текст у синьому кольорі!'));
+console.log(chalk2.green('Це текст у зеленому кольорі!'));
+console.log(chalk2.red('Це текст у червоному кольорі!'));
