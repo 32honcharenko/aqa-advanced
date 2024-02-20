9ad230a6b6a6b686eb36cde7e9a3dd235df4ec09
serhii honcharenko <32honcharenko@gmail.com>
2024-02-20 10:26:31 +0200

    lesson8

---
+++ lesson8/1.js
@@ -0,0 +1,8 @@
+// Імперативний код
+var numbers = [1, 2, 3, 4, 5];
+var squaredNumbers = [];
+
+for (var i = 0; i < numbers.length; i++) {
+    squaredNumbers.push(numbers[i] * numbers[i]);
+}
+console.log (squaredNumbers)
