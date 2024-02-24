b827c66642ee4e3ae696a6ba5fdd4017c8cae02a
serhii honcharenko <32honcharenko@gmail.com>
2024-02-24 19:32:56 +0200

    hw9

---
+++ hwLesson9/task2.js
@@ -0,0 +1,10 @@
+const book = {
+    title: "Histori",
+    author: "Honcharenko",
+    year: 2024,
+}
+
+let {title, author} = book
+
+console.log(title)
+console.log(author)
---
+++ hwLesson9/task4.js
@@ -0,0 +1,10 @@
+const person = {
+    firstName: "Serhii",
+    lastName: "Honcharenko",
+    age: 29,
+}
+
+person.email = "blabla@gmail.com"
+delete person.age
+
+console.log(person)
